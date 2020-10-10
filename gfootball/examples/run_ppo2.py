f235e790db59b5ec4c81a0439274ab52e0f1fba4
Jaime Rodriguez Lara <jaime.rodriguezlara@lumen.com.au>
2020-10-10 19:33:58 +1100

    commit

--- gfootball/examples/run_ppo2.py
+++ gfootball/examples/run_ppo2.py
@@ -101,7 +101,7 @@ def train(_):
   config.gpu_options.allow_growth = True
   tf.Session(config=config).__enter__()
 
-  ppo2.learn(network=FLAGS.policy,
+  a = ppo2.learn(network=FLAGS.policy,
              total_timesteps=FLAGS.num_timesteps,
              env=vec_env,
              seed=FLAGS.seed,
@@ -116,6 +116,8 @@ def train(_):
              save_interval=FLAGS.save_interval,
              cliprange=FLAGS.cliprange,
              load_path=FLAGS.load_path)
+
+  a.save("./")
 
 if __name__ == '__main__':
   app.run(train)
